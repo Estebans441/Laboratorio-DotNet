100320709ad6c006ab62560a03c0cfc151ca9f38
juanfra312003 <juanframireze@javeriana.edu.co>
2024-10-14 15:46:47 -0500

    :tada: Create VS Project

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,23 +1,24 @@
--- Create the new database
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'arq_personas')
+-- Otorgar propiedad de la base de datos al usuario sa
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'persona_db')
 BEGIN
-    CREATE DATABASE arq_personas;
+    CREATE DATABASE persona_db;
+    ALTER AUTHORIZATION ON DATABASE::persona_db TO sa;
 END
 GO
 
--- Switch to the new database
-USE arq_personas;
+-- Cambiar al nuevo contexto de la base de datos
+USE persona_db;
 GO
 
--- Create the schema
+-- Crear el esquema
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'arq_per_db')
 BEGIN
-    EXEC('CREATE SCHEMA arq_per_db');
+    EXEC('CREATE SCHEMA arq_per_db AUTHORIZATION sa');
 END
 GO
 
 -- -----------------------------------------------------
--- Table `arq_per_db`.`persona`
+-- Tabla `arq_per_db`.`persona`
 -- -----------------------------------------------------
 IF OBJECT_ID('arq_per_db.persona', 'U') IS NULL
 BEGIN
@@ -33,7 +34,7 @@ END
 GO
 
 -- -----------------------------------------------------
--- Table `arq_per_db`.`profesion`
+-- Tabla `arq_per_db`.`profesion`
 -- -----------------------------------------------------
 IF OBJECT_ID('arq_per_db.profesion', 'U') IS NULL
 BEGIN
@@ -47,7 +48,7 @@ END
 GO
 
 -- -----------------------------------------------------
--- Table `arq_per_db`.`estudios`
+-- Tabla `arq_per_db`.`estudios`
 -- -----------------------------------------------------
 IF OBJECT_ID('arq_per_db.estudios', 'U') IS NULL
 BEGIN
@@ -64,7 +65,7 @@ END
 GO
 
 -- -----------------------------------------------------
--- Table `arq_per_db`.`telefono`
+-- Tabla `arq_per_db`.`telefono`
 -- -----------------------------------------------------
 IF OBJECT_ID('arq_per_db.telefono', 'U') IS NULL
 BEGIN
