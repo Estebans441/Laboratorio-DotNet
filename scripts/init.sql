d34ab0c7e457fccf3177a01f5b5142a9d52f3c81
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-10-14 18:30:59 -0500

    :recycle: change db name

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,27 +1,27 @@
 -- Create the new database
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'arq_personas')
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'persona_db')
 BEGIN
-    CREATE DATABASE arq_personas;
+    CREATE DATABASE persona_db;
 END
 GO
 
 -- Switch to the new database
-USE arq_personas;
+USE persona_db;
 GO
 
 -- Create the schema
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'arq_per_db')
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'persona_db')
 BEGIN
-    EXEC('CREATE SCHEMA arq_per_db');
+    EXEC('CREATE SCHEMA persona_db');
 END
 GO
 
 -- -----------------------------------------------------
--- Table arq_per_db.persona
+-- Table persona_db.persona
 -- -----------------------------------------------------
-IF OBJECT_ID('arq_per_db.persona', 'U') IS NULL
+IF OBJECT_ID('persona_db.persona', 'U') IS NULL
 BEGIN
-    CREATE TABLE arq_per_db.persona (
+    CREATE TABLE persona_db.persona (
         cc INT NOT NULL,
         nombre VARCHAR(45) NOT NULL,
         apellido VARCHAR(45) NOT NULL,
@@ -33,11 +33,11 @@ END
 GO
 
 -- -----------------------------------------------------
--- Table arq_per_db.profesion
+-- Table persona_db.profesion
 -- -----------------------------------------------------
-IF OBJECT_ID('arq_per_db.profesion', 'U') IS NULL
+IF OBJECT_ID('persona_db.profesion', 'U') IS NULL
 BEGIN
-    CREATE TABLE arq_per_db.profesion (
+    CREATE TABLE persona_db.profesion (
         id INT NOT NULL,
         nom VARCHAR(90) NOT NULL,
         des TEXT NULL,
@@ -47,33 +47,33 @@ END
 GO
 
 -- -----------------------------------------------------
--- Table arq_per_db.estudios
+-- Table persona_db.estudios
 -- -----------------------------------------------------
-IF OBJECT_ID('arq_per_db.estudios', 'U') IS NULL
+IF OBJECT_ID('persona_db.estudios', 'U') IS NULL
 BEGIN
-    CREATE TABLE arq_per_db.estudios (
+    CREATE TABLE persona_db.estudios (
         id_prof INT NOT NULL,
         cc_per INT NOT NULL,
         fecha DATE NULL,
         univer VARCHAR(50) NULL,
         CONSTRAINT PK_estudios PRIMARY KEY (id_prof, cc_per),
-        CONSTRAINT FK_estudios_persona FOREIGN KEY (cc_per) REFERENCES arq_per_db.persona(cc),
-        CONSTRAINT FK_estudios_profesion FOREIGN KEY (id_prof) REFERENCES arq_per_db.profesion(id)
+        CONSTRAINT FK_estudios_persona FOREIGN KEY (cc_per) REFERENCES persona_db.persona(cc),
+        CONSTRAINT FK_estudios_profesion FOREIGN KEY (id_prof) REFERENCES persona_db.profesion(id)
     );
 END
 GO
 
 -- -----------------------------------------------------
--- Table arq_per_db.telefono
+-- Table persona_db.telefono
 -- -----------------------------------------------------
-IF OBJECT_ID('arq_per_db.telefono', 'U') IS NULL
+IF OBJECT_ID('persona_db.telefono', 'U') IS NULL
 BEGIN
-    CREATE TABLE arq_per_db.telefono (
+    CREATE TABLE persona_db.telefono (
         num VARCHAR(15) NOT NULL,
         oper VARCHAR(45) NOT NULL,
         duenio INT NOT NULL,
         CONSTRAINT PK_telefono PRIMARY KEY (num),
-        CONSTRAINT FK_telefono_persona FOREIGN KEY (duenio) REFERENCES arq_per_db.persona(cc)
+        CONSTRAINT FK_telefono_persona FOREIGN KEY (duenio) REFERENCES persona_db.persona(cc)
     );
 END
 GO
