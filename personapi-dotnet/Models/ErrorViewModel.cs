6118cb4d09520c6441797c1a4b5be6a7a06e7d49
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-10-14 18:57:56 -0500

    :recycle: specify database connection in project

--- personapi-dotnet/Models/ErrorViewModel.cs
+++ personapi-dotnet/Models/ErrorViewModel.cs
@@ -1,3 +1,4 @@
+// Scaffold-DbContext "Server=localhost,1433;Database=persona_db;User Id=sa;Password=Password123;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models/Entities
 namespace personapi_dotnet.Models
 {
     public class ErrorViewModel
